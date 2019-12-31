9c9b7b70d48c28ffd86973a483fa35d6e7922d20
Berlin Chan <berlinchancom@gmail.com>
2019-12-31 09:29:12 +0800

    init

---
+++ source/site/06-gallery-v1/myself/2004-12-19_17birthday/data.js
@@ -0,0 +1,23 @@
+var subTitle = '我17岁生日2004-12-19'
+var imageDB = new Array(
+  'imag001.jpg',
+  '',
+  'imag002.jpg',
+  '',
+  'imag003.jpg',
+  '',
+  'imag004.jpg',
+  '',
+  'imag005.jpg',
+  '',
+  'imag006.jpg',
+  '',
+  'imag007.jpg',
+  '',
+  'imag008.jpg',
+  '',
+  'imag009.jpg',
+  '',
+  'imag010.jpg',
+  ''
+)
